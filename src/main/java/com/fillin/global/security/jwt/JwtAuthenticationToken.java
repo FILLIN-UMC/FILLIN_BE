5e037f5a42e3140151b344c19fb3db7d079e325d
Jongchanpark22 <22channy@naver.com>
2026-01-09 22:46:14 +0900

    로그인 작업 중 ing

---
+++ src/main/java/com/fillin/global/security/jwt/JwtAuthenticationToken.java
@@ -0,0 +1,34 @@
+package com.fillin.global.security.jwt;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+/**
+ *  액세스 토큰 검증 및 인증 여부 판단
+ */
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Long user_id;
+    private final String token;
+
+
+    // 인증 후(인증 완료) 토큰용 생성자
+    public JwtAuthenticationToken(Long user_id, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.token = null; //인증전에는 토큰을 들고 있지 않는다.
+        this.user_id = user_id;
+        super.setAuthenticated(true);
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user_id;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+}
---
+++ src/main/java/com/fillin/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,161 @@
+package com.fillin.global.security.jwt;
+
+
+import com.fillin.domain.Member;
+import com.fillin.global.apiPayload.code.ErrorCode;
+import com.fillin.global.security.exception.AuthException;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+/**
+ *  Jwt 생성, 검증, 파싱 Provider
+ */
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access-token-validity}")
+    private long accessTokenValidity;
+
+    @Value("${jwt.refresh-token-validity}")
+    private long refreshTokenValidity;
+
+    private JwtParser jwtParser;
+    private Key secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.secretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
+        this.jwtParser = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build();
+    }
+
+    public String createAccessToken(Member member, String phoneNumber) {
+        try {
+            return generateToken(member, phoneNumber, accessTokenValidity,"access");
+        } catch (JwtException e) {
+            throw new AuthException(ErrorCode.JWT_GENERATION_FAILED);
+        }
+    }
+
+    public String createRefreshToken(Member member, String phoneNumber) {
+        try {
+            return generateToken(member, phoneNumber, refreshTokenValidity, "refresh");
+        } catch (JwtException e) {
+            throw new AuthException(ErrorCode.JWT_GENERATION_FAILED);
+        }
+    }
+
+    private String generateToken(Member member, String phoneNumber, long validity, String category) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + validity);
+
+        return Jwts.builder()
+                .setSubject(phoneNumber)
+                .claim("member_id", member.getId())
+                .claim("category", category)      // "access" 또는 "refresh"
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String createAccessTokenByEmail(Member member, String email,LoginType loginType) {
+        try {
+            return generateTokenByEmail(member, email, accessTokenValidity, "access",loginType);
+        } catch (JwtException e) {
+            throw new AuthException(ErrorCode.JWT_GENERATION_FAILED);
+        }
+    }
+
+    public String createRefreshTokenByEmail(Member member, String email, LoginType loginType) {
+        try {
+            return generateTokenByEmail(member, email, refreshTokenValidity, "refresh",loginType);
+        } catch (JwtException e) {
+            throw new AuthException(ErrorCode.JWT_GENERATION_FAILED);
+        }
+    }
+
+    private String generateTokenByEmail(Member member, String email, long validity, String category, LoginType loginType) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + validity);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("user_id", member.getId())
+                .claim("category", category)
+                .claim("loginType", loginType.name())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        if (token == null || token.trim().isEmpty()) {
+            return false;
+        }
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7).trim();
+        }
+        try {
+            jwtParser.parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            log.info("Invalid JWT token", e);
+            return false;}
+    }
+
+    // JWT 의 payload(Claims 객체) 추출
+    public Claims parseClaims(String token) {
+        return jwtParser.parseClaimsJws(token).getBody();
+    }
+
+    public Claims getClaimsFromToken(String token) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7).trim();
+        }
+
+        try {
+            return jwtParser.parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            throw new AuthException(ErrorCode.JWT_EXPIRED_TOKEN);
+        } catch (JwtException e) {
+            throw new AuthException(ErrorCode.JWT_INVALID_TOKEN);
+        }
+    }
+
+    // JWT 발급 시 필요한 시크릿 키를 Key 객체로 변환 (서명 검증)
+    private Key getSigningKey() {
+        return this.secretKey;
+    }
+
+    public Long getMemberIdFromToken(String token) {
+        if (token == null || token.trim().isEmpty()) {
+            throw new AuthException(ErrorCode.JWT_TOKEN_NOT_FOUND);
+        }
+
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7).trim();
+        }
+        try {
+            return jwtParser.parseClaimsJws(token).getBody().get("user_id", Long.class);
+        } catch (ExpiredJwtException e) {
+            throw new AuthException(ErrorCode.JWT_EXPIRED_TOKEN);
+        } catch (JwtException e) {
+            throw new AuthException(ErrorCode.JWT_INVALID_TOKEN);
+        }
+    }
+}

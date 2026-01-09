d8893f18e711787d7c24f1f644e8f49073e10581
shinae <shinae1023@naver.com>
2026-01-09 17:46:23 +0900

    [Docs] .gitkeep 파일 생성

---
+++ src/main/java/com/fillin/dto/mypage/response/ProfileResponseDto.java
@@ -0,0 +1,18 @@
+package com.fillin.dto.mypage.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileResponseDto {
+    private String nickname;
+    private String profileImageUrl;
+    private List<String> ranks;
+}
---
+++ src/main/java/com/fillin/service/mypage/MypageService.java
@@ -0,0 +1,9 @@
+package com.fillin.service.mypage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MypageService {
+}

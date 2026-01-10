d15a47f7865ff7206b92ec224f891eb795582a20
shinae <shinae1023@naver.com>
2026-01-10 14:30:02 +0900

    [Refactor] .gitkeep 파일 삭제

--- src/main/java/com/fillin/dto/mypage/response/RankResponseDto.java
+++ src/main/java/com/fillin/dto/mypage/response/RankResponseDto.java
@@ -1,7 +1,6 @@
 package com.fillin.dto.mypage.response;
 
 import com.fillin.domain.enums.Achievement;
-import com.fillin.domain.enums.Rank;
 import com.fillin.domain.enums.rank.Boangwan;
 import com.fillin.domain.enums.rank.Haegyeolsa;
 import com.fillin.domain.enums.rank.Tamheomga;

bb174f24cf87f285b2fbe4ccf6c72f5837d0cc05
answjddn0607 <166084634+answjddn0607@users.noreply.github.com>
2026-01-12 14:59:24 +0900

    Refactor: 수정 controller 태그 및 설명 추가

--- src/main/java/com/fillin/controller/report/ReportController.java
+++ src/main/java/com/fillin/controller/report/ReportController.java
@@ -4,6 +4,8 @@ import com.fillin.dto.report.request.ReportCreateRequestDto;
 import com.fillin.global.apiPayload.code.ResultCode;
 import com.fillin.global.apiPayload.response.Response;
 import com.fillin.service.report.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "Report API", description = "제보 관련 API - by 문정우")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reports")
@@ -21,6 +24,7 @@ public class ReportController {
 
     private final ReportService reportService;
 
+    @Operation(summary = "제보 하기", description = "사용자가 새로운 제보를 등록합니다.")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Response<Long> createReport(
             @AuthenticationPrincipal UserDetails userDetails, // Security 설정 가정
